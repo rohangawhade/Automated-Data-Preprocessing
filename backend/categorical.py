f144435bf87e23e3f99b881a49e07ec7949bc7cd
rohangawhade <gawhaderohan@gmail.com>
2022-07-06 15:16:14 +0530

    cleaned backend code

--- backend/categorical.py
+++ backend/categorical.py
@@ -1,60 +1,48 @@
 import pandas as pd
-import numpy as np
 import xgboost
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import roc_auc_score
-
-# import matplotlib.pyplot as plt
-
-import pandas as pd
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import OneHotEncoder
-# from sklearn.pipeline import make_pipeline
-# from sklearn.model_selection import cross_val_score
-
 from sklearn.preprocessing import LabelEncoder
 Le = LabelEncoder()
 
 
 def isCategorical(col, n):
     """ Function to check if a column is contains categorical data """
-    l = len(pd.unique(col))
-    if((n//l)<(n*0.05)):
-      return False
+    leng = len(pd.unique(col))
+    if((n//leng) < (n*0.05)):
+        return False
     else:
-      return True
+        return True
 
 
 def predict_val(X, target, X_pred):
     """ Function to predict the null values for categorical columns """
-
     var_colums = [c for c in X.columns if c not in [target]]
-    
     y = X.loc[:, target]
     X = X.loc[:, var_colums]
-    
-    colTR = [c for c in X.columns if type(X[c].iloc[0]) == str] # converting only string columns to one hot vectors
+
+    # converting only string columns to one hot vectors
+    colTR = [c for c in X.columns if type(X[c].iloc[0]) == str]
+
     # print(colTR)
-    column_trans = make_column_transformer((OneHotEncoder(handle_unknown='ignore'), colTR), remainder='passthrough')
+    column_trans = make_column_transformer((OneHotEncoder(
+      handle_unknown='ignore'),
+      colTR), remainder='passthrough')
     X = column_trans.fit_transform(X)
 
     y = Le.fit_transform(y)
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
-    # print(X_train.shape, X_valid.shape, y_train.shape, y_valid.shape)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)  # noqa: E501
 
-    # Model Training 
+    # Model Training
     model_xgboost = xgboost.XGBClassifier()
 
     eval_set = [(X_valid, y_valid)]
-
     model_xgboost.fit(X_train, y_train, eval_set=eval_set)
-
 
     # Model Prediction
     X_pred = X_pred.loc[:, var_colums]
     X_pred = column_trans.transform(X_pred)
 
     Y_pred = model_xgboost.predict(X_pred)
-    print("Predicted values are: ", Y_pred)
     return Le.inverse_transform(Y_pred)
--- backend/continuous.py
+++ backend/continuous.py
@@ -1,32 +1,8 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# ### To Do
-# - Remove unique columns
-# - Scaling / Normalization
-# - outlier analysis => 1) scipy import stats; df[(np.abs(stats.zscore(df)) < 3).all(axis=1)] (for standardizing values into normal distribution), calc zscore and set threshold 2) IQR
-# - sklearn imputer - replace missing with mean/median/most frequent/constant OR
-# - replace missing/garbage by using model
-
-# # 1. Removing Unique Columns
-# 
-# #### Progress
-# - Checking which column has as many unique values in it as the total number of rows. Then dropping the column. Eg: Dropping ID columns.
-# 
-# #### Issues
-# - If a column has an empty value, the number of unique values decreases and is not equal to number of rows. Hence it is not dropped.
-# - In case a column isnt actually unique identifier eg: sales in different months. There maybe no same sales in two months. Therefore that important column will be dropped.
-# 
-# #### Solution
-# - Try to find correlation between target variable and predictor variables. If it is close to 0 then drop it.
-
 from fancyimpute import IterativeImputer
 from sklearn.preprocessing import RobustScaler
-from sklearn.neighbors import LocalOutlierFactor
-
+# from sklearn.neighbors import LocalOutlierFactor
 import pandas as pd
-# import glob
-import numpy as np
 import re
 
 
@@ -38,18 +14,18 @@ def checkNumeric(num):
 
 
 def convertToNumeric(data, col):
-    for k in range(len(data[col])):    
-        res=""
+    for k in range(len(data[col])):
+        res = ""
         for i in data[col][k]:
-            res+=re.sub("[`~!@#$=+%^&*_/?()-]|[a-zA-Z]", "", i)
-        res=float(res)
+            res += re.sub("[`~!@#$=+%^&*_/?()-]|[a-zA-Z]", "", i)
+        res = float(res)
         data.loc[k, col] = res
 
 
 def CheckUnique(data):
     col_names = data.columns.to_list()
-    print("Original Column Names")
-    print(col_names)
+    # print("Original Column Names")
+    # print(col_names)
     new_col = col_names.copy()
     for i in col_names:
         if data[i].count() == len(data[i].unique()):
@@ -57,45 +33,45 @@ def CheckUnique(data):
         else:
             isUniq = False
         if isUniq:
-            print("Dropping", i)
+            # print("Dropping", i)
             new_col.remove(i)
     return new_col
-
-# # 2. Scaling / Normalization
-# 
-# - In order to identify scale the data, we cannot use the usual scaling methods such as Min Max Scaler or Logarithmic Scaler because they will consider outliers into the data and scale accordingly.
-# - Therefore we use a Robust Scaling method where we find the interquartile range and scale according to the range so that the data is not affected by outliers.
 
 
 def GetNumColumns(data):
     col_names = data.columns.to_list()
     final_col_names = col_names.copy()
     for i in col_names:
-        if data.dtypes[i] == "float" or \
-            data.dtypes[i] == "int64" or \
-            data.dtypes[i] == "int32":
-            print("ALREADY NUMERIC:-", i)
+        if data.dtypes[i] == "float" or data.dtypes[i] == "int64" or data.dtypes[i] == "int32":  # noqa: E501
+            # print("ALREADY NUMERIC:-", i)
             continue
         else:
-            c=0
-            ct=[]
+            c = 0
+            ct = []
             ct.append(data[i][:50])
             for j in ct[0]:
                 if checkNumeric(j):
-                    c=c+1
-            if c>40:
-                print("Numeric:-",i)
+                    c = c+1
+            if c > 40:
+                # print("Numeric:-", i)
                 convertToNumeric(data, i)
-                print("Converted Values of", i)
+                # print("Converted Values of", i)
             else:
                 final_col_names.remove(i)
-                print("Not a Numeric:-",i)
-            print("-----------")
-    # for i in col_names:
-    #     if str(data[i].dtypes) == "object":
-    #         final_col_names.remove(i)
-    print("These are the final numeric columns", final_col_names)
+                # print("Not a Numeric:-", i)
+            # print("-----------")
+    # print("These are the final numeric columns", final_col_names)
     return final_col_names
+
+
+'''
+Scaling / Normalization
+In order to identify scale the data, we cannot use the usual scaling methods
+such as Min Max Scaler or Logarithmic Scaler because they will consider
+outliers into the data and scale accordingly.
+Therefore we use a Robust Scaling method where we find the interquartile range
+and scale according to the range so that the data is not affected by outliers.
+'''
 
 
 def Scaling(data):
@@ -107,65 +83,72 @@ def Scaling(data):
     return df
 
 
-# # 3. Outlier Analysis
-# 
-# - The local outlier factor, or LOF for short, is a technique that attempts to harness the idea of nearest neighbors for outlier detection.
-# - The IQR can be used to identify outliers by defining limits on the sample values that are a factor k of the IQR below the 25th percentile or above the 75th percentile. The common value for the factor k is the value 1.5
+'''
+3. Outlier Analysis
+The local outlier factor, or LOF for short, is a technique that attempts
+to harness the idea of nearest neighbors for outlier detection.
+The IQR can be used to identify outliers by defining limits on the sample
+values that are a factor k of the IQR below the 25th percentile or
+above the 75th percentile. The common value for the factor k is the value 1.5
+'''
+
 
 def remove_outlier(DF, col_name):
     q1 = DF[col_name].quantile(0.25)
     q3 = DF[col_name].quantile(0.75)
-    print("25th Percentile of Numerical Attributes")
-    print(q1)
-    print("----------------------------")
-    print("75th Percentile of Numerical Attributes")
-    print(q3)
-    print("----------------------------")
-    IQR = q3-q1 # Interquartile range
-    print("Interquartile Range")
-    print(IQR)
-    d = DF[~((DF[col_name] < (q1 - 1.5 * IQR)) |(DF[col_name] > (q3 + 1.5 * IQR))).any(axis=1)]
-    return DF
+    # print("25th Percentile of Numerical Attributes")
+    # print(q1)
+    # print("----------------------------")
+    # print("75th Percentile of Numerical Attributes")
+    # print(q3)
+    # print("----------------------------")
+    IQR = q3-q1  # Interquartile range
+    # print("Interquartile Range")
+    # print(IQR)
+    d = DF[~((DF[col_name] < (q1 - 1.5 * IQR)) | (DF[col_name] > (q3 + 1.5 * IQR))).any(axis=1)]  # noqa: E501
+    return d
 
 
 def OutlierAnalysis(path):
     data = pd.read_csv(path)  # Reading dataset
-    print("----------------------------")
-    print("Original Dataset ")
-    print("----------------------------")
-    print(data)
-    print("----------------------------")
+    # print("----------------------------")
+    # print("Original Dataset ")
+    # print("----------------------------")
+    # print(data)
+    # print("----------------------------")
     col_name = CheckUnique(data)  # Removing unique columns
     data = data[col_name]  # Columns names which are not unique
-    
-    print("----------------------------")
-    print("Count of null values in continuous attributes")
-    print(data.isna().sum())
+
+    # print("----------------------------")
+    # print("Count of null values in continuous attributes")
+    # print(data.isna().sum())
     cols = GetNumColumns(data)  # get numerical columns
     mice_imputer = IterativeImputer()
     df2 = mice_imputer.fit_transform(data[cols])
     dd = {}
     for i in range(len(cols)):
-        dd[cols[i]] = df2[:,i].tolist()
+        dd[cols[i]] = df2[:, i].tolist()
     df4 = pd.DataFrame(dd)
     data[cols] = df4[cols]
 
-    print("----------------------------")
-    print("Count of null values AFTER removal in continuous attributes")
-    print(data.isna().sum())
-    data2 = data[cols]
-    print("----------------------------")
-    print("Data shape", data.shape)
-    print("Copied Data shape", data2.shape)
-    # lof = LocalOutlierFactor()
-    # yhat = lof.fit_predict(data2.values)  # LOF outlier remover
-    # mask = yhat != -1
-    # DF = data[mask]
-    # print("New Data shape", DF.shape)
-    
     # print("----------------------------")
-    # dd = DF.copy()
-    # dd = remove_outlier(DF, cols)  # Quartile outlier remover
+    # print("Count of null values AFTER removal in continuous attributes")
+    # print(data.isna().sum())
+    # data2 = data[cols]
+    # print("----------------------------")
+    # print("Data shape", data.shape)
+    # print("Copied Data shape", data2.shape)
+
+    '''
+    lof = LocalOutlierFactor()
+    yhat = lof.fit_predict(data2.values)  # LOF outlier remover
+    mask = yhat != -1
+    DF = data[mask]
+    print("New Data shape", DF.shape)
+    print("----------------------------")
+    dd = DF.copy()
+    dd = remove_outlier(DF, cols)  # Quartile outlier remover
+    '''
     data = Scaling(data)  # Scaling Dataset
     dd = remove_outlier(data, cols)  # Removing outliers after scaling
     return dd
@@ -173,10 +156,7 @@ def OutlierAnalysis(path):
 
 def ContinuousPreProcess(path):
     final_data = OutlierAnalysis(path)
-    print("----------------------------")
-    print("Processed Dataset:")
-    print("----------------------------")
+    # print("----------------------------")
+    # print("Processed Dataset:")
+    # print("----------------------------")
     return final_data
-
-# Test run
-# print(ContinuousPreProcess("D:\\Github Repositories\\AutoML\\data-cleaner\\notebooks\\updated_bike_buyers.csv"))
